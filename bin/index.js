081b9e8892bb7300375e62edbe45b1cfce4367d4
Retro <htoomyataung382@gmail.com>
2022-05-24 19:16:21 +0630

    update

--- bin/index.js
+++ bin/index.js
@@ -6,15 +6,24 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 const { execSync } = require('node:child_process')
 const usage = "\n Usage <app_name> create a boilerplate code for discord.js"
 const process = require('node:process')
-const options = yargs
+const dirName = yargs
     .usage(usage)
-    .option("i", { alias: "input", describe: "File you want to create", type: "string", demandOption: true })
+    .option("d", { alias: "dir", describe: "File you want to create", type: "string", demandOption: true })
+    .help(true)
+    .argv;
+const extraPkg = yargs
+    .usage(usage)
+    .option('p', { alias: "packages", describe: "Packges you want", type: "string", demandOption: false })
     .help(true)
     .argv;
 (async () => {
-    console.log(chalk.blue('Starting to clone from github into ' + yargs.argv.i))
-    execSync(`git clone https://github.com/retrouser955/discord.js-boilerplate.git ${yargs.argv.i}`)
+    console.log(chalk.blue('Starting to clone from github into ' + yargs.argv.d))
+    execSync(`git clone https://github.com/retrouser955/discord.js-boilerplate.git ${yargs.argv.d}`)
     console.log(chalk.blue(`Installing packages`))
-    execSync(`cd ${process.cwd()}/${yargs.argv.i} && npm install`)
-    console.log(chalk.green(`Finished installing packages! Your bot is now ready`))
+    execSync(`cd ${process.cwd()}/${yargs.argv.d} && npm install`)
+    console.log(chalk.green(`Finished installing packages!`))
+    if(!yargs.argv.p) return console.log(chalk.green(`Process finished! Code is generated at '${yargs.argv.d}'`))
+    console.log(chalk.blue(`Started installing extra packages`))
+    execSync(`cd ${process.cwd()}/${yargs.argv.d} && npm install ${yargs.argv.p}`)
+    console.log(chalk.green(`Process finished! Code is generated at '${yargs.argv.d}'`))
 })()
--- readme.md
+++ readme.md
@@ -11,5 +11,14 @@ npm install -g create-discord-boilerplate
 # Usage
 
 ```bash
-create-discord-boilerplate -i "myDiscordBot"
+create-discord-boilerplate -d "myDiscordBot"
+```
+
+# Adding extra packages
+
+create-discord-boilerplate provide a easy way to install extra packages while creating boilerplate
+
+```bash
+create-discord-boilerplate -d "myDiscordBot" -p "discord-player discord-easy-leveling"
+# discord-player is a music library for discord.js while discord-easy-leveling is a ranking package
 ```
