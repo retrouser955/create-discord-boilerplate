1c55a3f502686f9e17d5825913fa992541751200
Retro <htoomyataung382@gmail.com>
2022-05-24 18:49:44 +0630

    🚀 | beta

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,7 @@ const chalk = require('chalk')
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 const { execSync } = require('node:child_process')
 const usage = "\n Usage <app_name> create a boilerplate code for discord.js"
+const process = require('node:process')
 const options = yargs
     .usage(usage)
     .option("i", { alias: "input", describe: "File you want to create", type: "string", demandOption: true })
@@ -13,8 +14,7 @@ const options = yargs
 (async () => {
     console.log(chalk.blue('Starting to clone from github into ' + yargs.argv.i))
     execSync(`git clone https://github.com/retrouser955/discord.js-boilerplate.git ${yargs.argv.i}`)
-    execSync(`cd ${yargs.argv.i}`)
     console.log(chalk.blue(`Installing packages`))
-    execSync(`npm install`)
+    execSync(`cd ${process.cwd()}/${yargs.argv.i} && npm install`)
     console.log(chalk.green(`Finished installing packages! Your bot is now ready`))
 })()
