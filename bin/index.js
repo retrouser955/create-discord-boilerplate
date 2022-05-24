a37fba816b796622cecec5faca6a99fc20a9c565
Retro <htoomyataung382@gmail.com>
2022-05-24 16:57:38 +0630

    test

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,18 @@
 #! /usr/bin/env node
 const yargs = require("yargs")
-const utils = require('./utils.js')
+// const utils = require('./utils.js')
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+const { exec } = require('node:child_process')
 const usage = "\n Usage <app_name> create a boilerplate code for discord.js"
 const options = yargs
     .usage(usage)
     .option("i", { alias: "input", describe: "File you want to create", type: "string", demandOption: true })
     .help(true)
     .argv;
-utils.createApp(yargs.argv.i)
+(async () => {
+    exec(`git clone https://github.com/retrouser955/discord.js-boilerplate.git ${yargs.argv.i}`)
+    await delay(4000)
+    exec(`cd ${yargs.argv.i}`)
+    const data = exec(`npm install`)
+    data.stdout.on('data', (data) => { console.log(data) })
+})()
--- bin/utils.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require('child_process')
-async function createDiscordApp(folderName) {
-    console.log('Cloning the github repository ... \n \n $ git clone https://github.com/retrouser955/discord.js-boilerplate.git ${folderName}')
-    exec(`git clone https://github.com/retrouser955/discord.js-boilerplate.git ${folderName}`)
-    console.log(`\n Finished Cloning the boilerplate github repo`)
-}
-module.exports = {
-    createApp: createDiscordApp
-}
