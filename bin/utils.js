e98ed58b36f18b204e1f820592bab6cb95ef308f
Retro <htoomyataung382@gmail.com>
2022-05-24 12:46:11 +0630

    update

--- bin/utils.js
+++ bin/utils.js
@@ -1,10 +1,8 @@
 const { exec } = require('child_process')
 async function createDiscordApp(folderName) {
-    console.log('Cloning the github repository ...')
-    const cloning =  exec(`git clone https://github.com/retrouser955/discord.js-boilerplate.git ${folderName}`)
-    cloning.stdout.on('data', (data) => {
-        console.log(data)
-    })
+    console.log('Cloning the github repository ... \n \n $ git clone https://github.com/retrouser955/discord.js-boilerplate.git ${folderName}')
+    exec(`git clone https://github.com/retrouser955/discord.js-boilerplate.git ${folderName}`)
+    console.log(`\n Finished Cloning the boilerplate github repo`)
 }
 module.exports = {
     createApp: createDiscordApp
